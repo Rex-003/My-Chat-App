ad65bac4eb2f319d0b9b90f1f237f7b46575cc8a
RAJAT <rajatchahar.x@gmail.com>
2026-07-23 21:36:52 +0530

    Set the Env Variable

--- my-chat-app-server/app.js
+++ my-chat-app-server/app.js
@@ -18,8 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin:
-      "https://my-chat-app-client-46cd.onrender.com" || "http://localhost:5173",
+    origin: process.env.CLIENT_URI || "http://localhost:5173",
     credentials: true,
   }),
 );
@@ -269,7 +268,7 @@ const server = app.listen(port, () => {
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: process.env.CLIENT_URL || "https://my-chat-app-tam8.onrender.com",
+    origin: process.env.CLIENT_URI,
     methods: ["GET", "POST"],
   },
 });
