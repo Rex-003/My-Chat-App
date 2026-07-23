5b83e271fd7a4b49fddc2ca8ee9563eb47f3026a
RAJAT <rajatchahar.x@gmail.com>
2026-07-23 21:19:10 +0530

    Change the faulty cors error

--- my-chat-app-server/app.js
+++ my-chat-app-server/app.js
@@ -18,7 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin:
+      "https://my-chat-app-client-46cd.onrender.com" || "http://localhost:5173",
     credentials: true,
   }),
 );
